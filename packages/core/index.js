cb96372e375876280e84989559096fe4dce3e65e
Thijs Louisse <Thijs.Louisse@ing.com>
2021-05-18 12:31:04 +0200

    core types tmp

--- packages/core/index.js
+++ packages/core/index.js
@@ -1,55 +1,47 @@
-// lit-element
-export {
-  css,
-  CSSResult,
-  // decorators.js
-  // customElement,
-  // updating-element.js
-  defaultConverter,
-  // eventOptions,
-  LitElement,
-  notEqual,
-  // property,
-  // query,
-  // queryAll,
-  // css-tag.js
-  supportsAdoptingStyleSheets,
-  unsafeCSS,
-  UpdatingElement,
-} from 'lit';
+// // lit-element
+// export {
+//   css,
+//   CSSResult,
+//   // decorators.js
+//   // customElement,
+//   // updating-element.js
+//   defaultConverter,
+//   // eventOptions,
+//   LitElement,
+//   notEqual,
+//   // property,
+//   // query,
+//   // queryAll,
+//   // css-tag.js
+//   supportsAdoptingStyleSheets,
+//   unsafeCSS,
+//   UpdatingElement,
+// } from 'lit';
 
 // lit-html
-export {
-  // AttributePart,
-  // BooleanAttributePart,
-  // directive,
-  // EventPart,
-  html,
-  // isDirective,
-  // isPrimitive,
-  noChange,
-  // NodePart,
-  nothing,
-  // PropertyPart,
-  render,
-  svg, // SVGTemplateResult, // TemplateResult, // reparentNodes, // removeNodes,
-} from 'lit';
+export * from 'lit';
 
-export { directive, AsyncDirective } from 'lit/async-directive.js';
+export * from 'lit/decorators.js';
+export * from 'lit/directive.js';
+export * from 'lit/async-directive.js';
+export * from 'lit/html.js';
+export * from 'lit/polyfill-support.js';
 
-export { isDirectiveResult } from 'lit/directive-helpers.js';
+// export * from 'lit/async-directive.js';
 
-export { asyncAppend } from 'lit/directives/async-append.js';
-export { asyncReplace } from 'lit/directives/async-replace.js';
-export { cache } from 'lit/directives/cache.js';
-export { classMap } from 'lit/directives/class-map.js';
-export { guard } from 'lit/directives/guard.js';
-export { ifDefined } from 'lit/directives/if-defined.js';
-export { repeat } from 'lit/directives/repeat.js';
-export { styleMap } from 'lit/directives/style-map.js';
-export { unsafeHTML } from 'lit/directives/unsafe-html.js';
-export { until } from 'lit/directives/until.js';
-export { isTemplateResult } from 'lit/directive-helpers.js';
+// export * from 'lit/directive-helpers.js';
+
+// export * from 'lit/directives/async-append.js';
+// export { asyncReplace } from 'lit/directives/async-replace.js';
+// export { cache } from 'lit/directives/cache.js';
+// export { classMap } from 'lit/directives/class-map.js';
+// export { guard } from 'lit/directives/guard.js';
+// export { ifDefined } from 'lit/directives/if-defined.js';
+// export { repeat } from 'lit/directives/repeat.js';
+// export { styleMap } from 'lit/directives/style-map.js';
+// export { unsafeHTML } from 'lit/directives/unsafe-html.js';
+// export { until } from 'lit/directives/until.js';
+// export { isTemplateResult } from 'lit/directive-helpers.js';
 
 // export { render as renderShady } from 'lit-html/lib/shady-render.js';
 // open-wc
